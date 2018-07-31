284a1c0c345cc80447fd29e57c74bc077a869ef2
MarynaKontar <marina.acoustic@gmail.com>
2018-07-31 19:53:27 +0300

    replace baseUrl from 'http://' with 'https://' because when make gh-pages there was error (mixed content)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   appId: '2fc48e95517bffcce3aea91a895ee238',
-  baseUrl: 'http://'
+  baseUrl: 'https://'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appId: '2fc48e95517bffcce3aea91a895ee238',
-  baseUrl: 'http://'
+  baseUrl: 'https://'
 };
 
 /*
